efa2e263e3eb07d666f980a0876d6c7a397645aa
eeva <t2roee00@students.oamk.fi>
2024-01-25 16:34:39 +0200

    valamis_ei testattu

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import Login from './Screens/Login'
+import { PaperProvider } from 'react-native-paper';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <Login />
+      </PaperProvider>
   );
 }
 
---
+++ Screens/Login.js
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { Appbar } from 'react-native-paper';
+import MainAppbar from '../Components/MainAppbar';
+
+export default function Login() {
+  const [formData, setFormData] = useState({username: '', password: ''})
+  const [showError, setShowError] = useState(false)
+
+  const validateAndSubmit = () => {
+    setShowError(true)
+    if (formData.username.lenght >0 && formData.password.lenght >8){
+      setFormData({username: '',password: ''})
+      setShowError(false)
+    }
+  }
+  return (
+    <>  
+  <MainAppbar title="Login"/>
+  <View style={styles.container}>
+    <TextInput label= "Username" 
+    style={styles.input_field}
+    value={FormData.username}
+    onChangeText={text => setFormData({...FormData,username: text})}
+    />
+    <TextInput label="Password" style={styles.input_field} 
+    keyboardType= 'visible-password' 
+    value={FormData.password}
+    onChangeText={text0> setFormData({...FormData,password: text})}
+    />
+   
+    <Button mode= "contained" style={styles.button}>Submit</Button>
+  </View>
+  </>
+)
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: ' center',
+  },
+  input_field: {
+    backgroudColor: '#fcfcfc',
+    margin: (16,8,16,8,)
+  },
+  button: {
+    margin: (16,8,16,8)
+  }
+  
+});
